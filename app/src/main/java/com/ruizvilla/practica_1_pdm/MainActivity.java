ae6d42da2fb244ea0436455832e04269c3e4b0b6
SamuelRuiz <samuel.ruiz@udea.edu.co>
2017-02-28 14:11:22 -0500

    V0. Inicio proyecto en limpio a partir de otra version experimental

---
+++ app/src/main/java/com/ruizvilla/practica_1_pdm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ruizvilla.practica_1_pdm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
