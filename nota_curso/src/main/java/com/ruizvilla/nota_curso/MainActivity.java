f9b1b6aad5403f018c2fed0bb666290faeea41d4
SamuelRuiz <samuel.ruiz@udea.edu.co>
2017-03-02 08:26:26 -0500

    V5.2 Nota_curso: Aplicacion  respondiendo a todos los requerimientos

--- nota_curso/src/main/java/com/ruizvilla/nota_curso/MainActivity.java
+++ nota_curso/src/main/java/com/ruizvilla/nota_curso/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText editText_1, editText_2,editText_3, editText_4,editText_5, editText_6;
     Button button_1;
-    Double calificacion, a_1,a_2,a_3,a_4,a_5,a_6;
+    Double calificacion, a_1,a_2,a_3,a_4,a_5;
 
 
 
@@ -37,16 +37,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //______________Entres estos dos segmentos programo lo que sucede al accionarn el boton
-                if (!(editText_1.getText().toString().equals(""))) {
-                    a_1 = Double.parseDouble(editText_1.getText().toString());
 
-                    calificacion = a_1 * 2;
-                    editText_6.setText(String.valueOf(calificacion));
+                a_1 = Double.parseDouble(editText_1.getText().toString());
+                a_2 = Double.parseDouble(editText_2.getText().toString());
+                a_3 = Double.parseDouble(editText_3.getText().toString());
+                a_4 = Double.parseDouble(editText_4.getText().toString());
+                a_5 = Double.parseDouble(editText_5.getText().toString());
+
+                calificacion = (a_1 * 0.6)+(a_2 * 0.05)+(a_3 * 0.07)+(a_4 * 0.08)+(a_5 * 0.2);
+                editText_6.setText(String.valueOf(calificacion));
+                // Hasta aca listo el calculo de la nota
+
+                if (calificacion<=1) {
+                    Toast.makeText(getApplicationContext(), "Estas en el lugar equivocado", Toast.LENGTH_SHORT).show();
+                } else {
+                    if(calificacion>=1.1 && calificacion<=2){
+                        Toast.makeText(getApplicationContext(), "Remal", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    if(calificacion>=2 && calificacion<=3){
+                        Toast.makeText(getApplicationContext(), "Es posible recuperarse", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if(calificacion>=3.1 && calificacion<=4){
+                            Toast.makeText(getApplicationContext(), "Normalito", Toast.LENGTH_SHORT).show();
+                        }else{
+                            if(calificacion>=4.1 && calificacion<=4.5){
+                                Toast.makeText(getApplicationContext(), "Muy bien", Toast.LENGTH_SHORT).show();
+                            }else{
+                                if(calificacion>=4.6 && calificacion<=5) {
+                                    Toast.makeText(getApplicationContext(), "Excelente estudiante", Toast.LENGTH_SHORT).show();
+                                }
+                                }
+                            }
+                        }
+                    }
+                }
                 }
 
-
                 //________________________________________________________________
-            }
-    });
-  }
+    }); //Fin de boton
+  } // Fin de OnCreate
 } // Llave mas externa
